5c79d42f871d84ed4c24205a42a32b9ab46f8868
Ethan <CengFuming@qq.com>
2024-06-29 01:55:09 +0800

    feat: 修改地址

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ethan-ceng.github.io/Panda-Book/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -17,6 +17,8 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Ethan-Ceng', // Usually your GitHub org/user name.
   projectName: 'Panda-Book', // Usually your repo name.
+  deploymentBranch: 'master', //分支
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
